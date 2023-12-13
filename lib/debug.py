151de0af7220f87efa14ff33f8c162b5f919d24c
Laboso2023 <denis.laboso@student.moringaschool.com>
2023-12-13 11:35:12 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
+from faker import Faker
+import random
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy_sandbox import Student
+from app.db import Game
 
-engine = create_engine('sqlite:///students.db')
+fake = Faker()
 
 if __name__ == '__main__':
+    
+    engine = create_engine('sqlite:///seed_db.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
     import ipdb; ipdb.set_trace()
